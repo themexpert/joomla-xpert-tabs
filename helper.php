306081e4ea141963518a34c7500abd3dd24927ae
Parvez <parvez@themexpert.com>
2013-01-06 14:08:44 +0800

    build file changed

--- helper.php
+++ helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package Xpert Tabs
- * @version 2.1
+ * @version ##VERSION##
  * @author ThemeXpert http://www.themexpert.com
  * @copyright Copyright (C) 2009 - 2011 ThemeXpert
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
