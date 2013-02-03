1d184e8865665221f65d68125cc08d4ff397adea
Parvez <parvez@themexpert.com>
2013-02-03 16:59:40 +0800

    XTABS-2 #resolve

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package Xpert Tabs
- * @version ##VERSION##
+ * @version 3.3
  * @author ThemeXpert http://www.themexpert.com
  * @copyright Copyright (C) 2009 - 2011 ThemeXpert
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
@@ -9,7 +9,7 @@
  */
 
 ?>
-<!--Xpert Tabs ##VERSION## by ThemeXpert(www.themexpert.com)- Start-->
+<!--Xpert Tabs 3.3 by ThemeXpert(www.themexpert.com)- Start-->
 <div id="<?php echo $module_id;?>" class="txtabs-wrap <?php echo $params->get('style','style1');?>">
 
     <?php if($tabs_position == 'top') echo $tabs_title;?>
@@ -19,8 +19,8 @@
 
             <?php $class = ($i == 0) ? ' active in' : '';?>
 
-            <div class="txtabs-pane<?php echo $class; ?> <?php echo $transition; ?> clearfix" id="<?php echo $module_id . '-'. $i; ?>">
-                <div class="txtabs-pane-in">
+            <div class="txtabs-pane<?php echo $class; ?> <?php echo $transition; ?>" id="<?php echo $module_id . '-'. $i; ?>">
+                <div class="txtabs-pane-in clearfix">
                     <?php echo $items[$i]->introtext; ?>
 
                     <?php if( $content_source != 'module' AND $params->get('readmore', 1) ) :?>
@@ -39,4 +39,4 @@
     <?php if($tabs_position == 'bottom') echo $tabs_title;?>
 
 </div>
-<!--Xpert Tabs ##VERSION## by ThemeXpert- End-->
+<!--Xpert Tabs 3.3 by ThemeXpert- End-->
